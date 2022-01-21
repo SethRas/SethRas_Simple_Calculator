d0a388ab9bc186716f432d6556c4b0fd4ee544e6
rasset69@gmail.com <rasset69@gmail.com>
2022-01-20 19:48:31 -0700

    Add code

--- SethRas_Simple_Calculator/simpleCalculator.vb
+++ SethRas_Simple_Calculator/simpleCalculator.vb
@@ -2,7 +2,7 @@
 'RCET 0265
 'Spring 2022
 'Simple Calculator
-'
+'https://github.com/SethRas/SethRas_Simple_Calculator.git
 
 Option Explicit On
 Option Strict On
@@ -10,6 +10,17 @@ Option Strict On
 Module simpleCalculator
 
     Sub Main()
+        Dim userInputA As String
+        Dim userInputC As String
+
+        Console.WriteLine("Hello, please enter a number")
+        userInputA = Console.ReadLine()
+
+        Console.WriteLine("Please enter another number")
+        userInputC = Console.ReadLine()
+
+        Console.WriteLine(CInt(userInputA) + CInt(userInputC))
+        Console.ReadLine()
 
     End Sub
 
