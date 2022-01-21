fc7534a1fafc4db7c5a13f715710db8ead6902c1
rasset69@gmail.com <rasset69@gmail.com>
2022-01-20 17:54:59 -0700

    Add project files.

---
+++ SethRas_Simple_Calculator/simpleCalculator.vb
@@ -0,0 +1,16 @@
+﻿'Seth N Rasmussen
+'RCET 0265
+'Spring 2022
+'Simple Calculator
+'
+
+Option Explicit On
+Option Strict On
+
+Module simpleCalculator
+
+    Sub Main()
+
+    End Sub
+
+End Module
