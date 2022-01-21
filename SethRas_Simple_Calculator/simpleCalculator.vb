8830a0e9fe895692d78ce280cd8cb6f4c63965ee
rasset69@gmail.com <rasset69@gmail.com>
2022-01-20 19:54:22 -0700

    Add comments

--- SethRas_Simple_Calculator/simpleCalculator.vb
+++ SethRas_Simple_Calculator/simpleCalculator.vb
@@ -6,19 +6,23 @@
 
 Option Explicit On
 Option Strict On
-
 Module simpleCalculator
 
     Sub Main()
+        'Declare inputs as string values I suppose you could 
+        'declare them as int as well?
         Dim userInputA As String
         Dim userInputC As String
 
+        'Greeting and prompt for initial input
         Console.WriteLine("Hello, please enter a number")
         userInputA = Console.ReadLine()
 
+        'Prompt for second number
         Console.WriteLine("Please enter another number")
         userInputC = Console.ReadLine()
 
+        'Create addition operator
         Console.WriteLine(CInt(userInputA) + CInt(userInputC))
         Console.ReadLine()
 
