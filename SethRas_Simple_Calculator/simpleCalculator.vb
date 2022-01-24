5a1af81c90e68d2845fcf3da702949e811a00b75
rasset69@gmail.com <rasset69@gmail.com>
2022-01-23 18:10:28 -0700

    Multiply or Add

--- SethRas_Simple_Calculator/simpleCalculator.vb
+++ SethRas_Simple_Calculator/simpleCalculator.vb
@@ -5,25 +5,36 @@
 'https://github.com/SethRas/SethRas_Simple_Calculator.git
 
 Option Explicit On
-Option Strict On
+Option Strict Off
+
 Module simpleCalculator
 
     Sub Main()
         'Declare inputs as string values I suppose you could 
         'declare them as int as well?
-        Dim userInputA As String
-        Dim userInputC As String
+        Dim userInputA As Integer
+        Dim userInputB As Integer
+        Dim choice As String
 
         'Greeting and prompt for initial input
+        Console.WriteLine("This Calculator will add or Multiply Two numbers")
         Console.WriteLine("Hello, please enter a number")
         userInputA = Console.ReadLine()
 
         'Prompt for second number
         Console.WriteLine("Please enter another number")
-        userInputC = Console.ReadLine()
+        userInputB = Console.ReadLine()
 
-        'Create addition operator
-        Console.WriteLine(CInt(userInputA) + CInt(userInputC))
+        'Prompt for Operator
+        Console.WriteLine("Select operation + or * ")
+        choice = Console.ReadLine
+
+        If choice = "+" Then
+            Console.WriteLine(userInputA & "+" & userInputB & "=" & userInputA + userInputB)
+        ElseIf choice = "*" Then
+            Console.WriteLine(userInputA & "*" & userInputB & "=" & userInputA * userInputB)
+        End If
+
         Console.ReadLine()
 
     End Sub
